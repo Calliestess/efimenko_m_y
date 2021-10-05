3bf1988651f29a5d859ff7fab7a7433be57932d2
Calliestess <90801166+Calliestess@users.noreply.github.com>
2021-10-06 01:17:21 +0300

    Good

---
+++ HW_first/level1ex2.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  List<int> input = [];
+
+  stdin.readLineSync()!.split(' ').forEach((element) {
+    input.add(int.parse(element));
+  });
+  int a = input[0];
+  int b = input[1];
+  int res = a*a + b*b;
+  print(res);
+}
